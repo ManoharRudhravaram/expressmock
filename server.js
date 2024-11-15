9cab057b4854626b7b75cd2b82600beac36a147d
manohar <manoharrudhravaram@gmail.com>
2024-11-15 23:23:24 +0530

    kjklknbhjknm

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ app.get('/api/hello', (req, res) => {
 });
 
 // Export the app to be used by Vercel
+module.exports = app;
+
+// Add this block for local development
 if (require.main === module) {
   app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
   });
 }
-
-module.exports = app;
